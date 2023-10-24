79399152b3a8e61991d0998daefaa1fa6bc08def
MenassieErmias <ermiasmenase@gmail.com>
2023-10-24 13:46:47 +0300

    tours, with readmore toggle

--- 02-tours/src/Tour.jsx
+++ 02-tours/src/Tour.jsx
@@ -1,6 +1,8 @@
 import React, { useState } from 'react';
 
 const Tour = ({ tour }) => {
+  const [readMore, setReadMore] = useState(false);
+
   return (
     <article className='single-tour'>
       <img src={tour.image} alt={tour.name} />
@@ -9,7 +11,17 @@ const Tour = ({ tour }) => {
           <h4>{tour.name}</h4>
           <p className='tour-price'>${tour.price}</p>
         </div>
-        <p>{tour.info}</p>
+        {
+          readMore ?
+            <p>
+              {tour.info}
+            </p>
+            :
+            <>
+              {tour.info.slice(0, 100)}
+              <button onClick={() => setReadMore(true)}>...</button>
+            </>
+        }
         <button className="delete-btn">
           not interested
         </button>
